c895e52547a4ae1acc9565f10d351f78e88a6b5f
Eduardo <198397564+esb-11@users.noreply.github.com>
2025-12-04 14:24:42 -0300

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -1,5 +1,6 @@
 class Program
 {
+    static public string[] commandsList = ["exit", "echo", "type"];
     static void Main()
     {
         string input = "";
@@ -22,22 +23,43 @@ class Program
     static void Evaluate(string input)
     {
         if (input.Length == 0) return;
-        
+
         string[] args = input.Split(" ");
         string command = args[0];
-        
+
         if (command == "echo")
         {
             Echo(args[1..]);
         }
+        else if (command == "type")
+        {
+            Type(args[1]);
+        }
         else
         {
-            Console.WriteLine($"{command}: command not found");
+            CommandNotFound(command);
         }
     }
 
     static void Echo(string[] args)
     {
         Console.WriteLine(string.Join(" ", args));
+    }
+
+    static void Type(string commandName)
+    {
+        if (commandsList.Contains(commandName))
+        {
+            Console.WriteLine($"{commandName} is a shell builtin");
+        }
+        else
+        {
+            CommandNotFound(commandName);
+        }
+    }
+
+    static void CommandNotFound(string command)
+    {
+        Console.WriteLine($"{command}: not found");
     }
 }
