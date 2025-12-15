d94659fbf748388b0247103deef0803a522bd5d9
Eduardo <198397564+esb-11@users.noreply.github.com>
2025-12-15 18:40:13 -0300

    codecrafters submit [skip ci]

--- src/Shell.cs
+++ src/Shell.cs
@@ -5,7 +5,7 @@ namespace src;
 class Shell
 {
     private Env Env { get; init; }
-    private Builtins  Builtins { get; init; }
+    private Builtins Builtins { get; init; }
     public string Command = "";
     public List<string> Arguments = [];
 
@@ -25,40 +25,16 @@ class Shell
 
     static private (string command, List<string> arguments) ParseInput(string input)
     {
-        StringBuilder sb = new();
-        string command = "";
-        List<string> arguments = [];
+        List<string> args = [.. input.Split(' ')];
+        args.RemoveAll((str) => string.IsNullOrEmpty(str));
 
-        for (int i = 0; i <= input.Length; i++)
-        {
-            char c = i < input.Length ? input[i] : ' ';
-            // when we find the first empty space, define the command as the current sb value
-            if (c == ' ' && command == "")
-            {
-                command = sb.ToString();
-                sb.Clear();
-            }
-            // after the command was found, start populating the arguments array
-            else if (c == ' ' && sb.Length > 0)
-            {
-                arguments.Add(sb.ToString());
-                sb.Clear();
-            }
-            else if (c != ' ')
-            {
-                sb.Append(c);
-            }
-        }
-
-        return (command, arguments);
+        return (args[0], args[1..]);
     }
 
     public string Eval()
     {
         if (Builtins.HasCommand(Command)) return Builtins.Run(Command, Arguments);
         else if (Env.HasProgram(Command)) return ExecuteProgram();
-        Command = "";
-        Arguments = [];
         return $"{Command}: not found";
     }
 
