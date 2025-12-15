c6b79c2b074349e176442b02c41f5ea2fe9edd52
Eduardo <198397564+esb-11@users.noreply.github.com>
2025-12-15 18:00:53 -0300

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -7,10 +7,10 @@ class Program
         Shell shell = new();
         while (shell.Command != "exit")
         {
-            if (shell.Command.Length == 0) continue;
             string result = shell.Eval();
             if (result.Length > 0) Console.WriteLine(result);
             shell.Read();
+            if (shell.Command.Length == 0) continue;
         }
     }
 }
